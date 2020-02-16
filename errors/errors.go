96fc49bf9a85a46030636347bf18205c1389acec
Young Jin Kim <youngjin.kim@universe.com>
2020-02-16 16:30:26 -0500

    Added error handling via error struct

--- errors/errors.go
+++ errors/errors.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 )
 
+type argError struct {
+	arg int
+	problem string
+}
+
 func positiveNum(arg int) (int, error) {
 	if arg < 0 {
 		return -1, errors.New("Not a positive number")
@@ -13,9 +18,29 @@ func positiveNum(arg int) (int, error) {
 	return arg, nil
 }
 
+func (e *argError) Error() string {
+	return fmt.Sprintf("%s", e.problem)
+}
+
+func positiveNum2(arg int) (int, error) {
+	if arg < 0 {
+		return -1, &argError{arg, "Not a positive number"}
+	}
+
+	return arg, nil
+}
+
 func main() {
 	for _, i := range []int{-1, 1} {
 		if n, e := positiveNum(i); e != nil {
+			fmt.Println("ERROR", e)
+		} else {
+			fmt.Println("POSITIVE:", n)
+		}
+	}
+
+	for _, i := range []int{-100, 23} {
+		if n, e := positiveNum2(i); e != nil {
 			fmt.Println("ERROR", e)
 		} else {
 			fmt.Println("POSITIVE:", n)
