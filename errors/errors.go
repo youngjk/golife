acaa18aa7b2524361627664c56d0225ffe4251ee
Young Jin Kim <youngjin.kim@universe.com>
2020-02-16 21:51:40 -0500

    Added extra practice on error handling with even number checker

--- errors/errors.go
+++ errors/errors.go
@@ -10,16 +10,16 @@ type argError struct {
 	problem string
 }
 
+func (e *argError) Error() string {
+	return fmt.Sprintf("%s", e.problem)
+}
+
 func positiveNum(arg int) (int, error) {
 	if arg < 0 {
 		return -1, errors.New("Not a positive number")
 	}
 
 	return arg, nil
-}
-
-func (e *argError) Error() string {
-	return fmt.Sprintf("%s", e.problem)
 }
 
 func positiveNum2(arg int) (int, error) {
@@ -29,6 +29,16 @@ func positiveNum2(arg int) (int, error) {
 
 	return arg, nil
 }
+
+func evenNumber(arg int) (int, error) {
+	if arg%2 != 0 {
+		return -1, &argError{arg, "Not a even number"}
+	} else {
+		return arg, nil
+	}
+}
+
+
 
 func main() {
 	for _, i := range []int{-1, 1} {
@@ -44,6 +54,14 @@ func main() {
 			fmt.Println("ERROR", e)
 		} else {
 			fmt.Println("POSITIVE:", n)
+		}
+	}
+
+	for _, i := range []int{23, 25, 26} {
+		if n, e := evenNumber(i); e != nil {
+			fmt.Println("ERROR", e)
+		} else {
+			fmt.Println("EVEN NUMBER:", n)
 		}
 	}
 }
